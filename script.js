a880847b4ec9676172a6e8868c5309b662bd481d
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2024-12-01 14:10:00 +0000

    Completed change the size of the square followed by the user input

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // create container
+let squarePerSide = prompt("The number of squares per side for the new grid");
+
 const container = document.createElement("div");
 container.classList.add("container");
 
 const body = document.querySelector("body");
 body.appendChild(container);
+
 
 // create square's container
 const squareContainer = document.createElement("div");
@@ -11,11 +14,26 @@ squareContainer.classList.add("square-container");
 container.appendChild(squareContainer);
 
 // create grid
-for (let i = 0; i < 16 * 16; i++) {
+for (let i = 0; i < squarePerSide * squarePerSide; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
+    div.style.width = `${960 / squarePerSide}px`;
+    div.style.height = `${960 / squarePerSide}px`;
     squareContainer.appendChild(div);
 }
 
 //add effect to the grid
-const square = querySelector("#square");
+squareContainer.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = randomColor();
+});
+
+
+//function random color
+function randomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
