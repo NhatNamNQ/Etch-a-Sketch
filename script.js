7cb3551ef72a29ffcbea208bc45332e382fd07f7
NhatNamNQ <138838322+NhatNamNQ@users.noreply.github.com>
2024-12-02 07:29:57 +0000

    Change the file script.js:
    - Add a button to change the size of grid
    - Add the event "click" to the button:
        + This will prompt the user input to change the size of the grid
        + Remove the default grid (16 x 16)
        + Create new grid having the user's size input
        + Add the effect hover
    - Add the function createSquares
    - Add function hovering
    - Add function removeSquares

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // create container
-let squarePerSide = prompt("The number of squares per side for the new grid");
-
 const container = document.createElement("div");
 container.classList.add("container");
 
 const body = document.querySelector("body");
 body.appendChild(container);
+
+// create button prompt the user
+const button = document.createElement("button");
+button.id = "btnSize";
+button.textContent = "Change size of the grid";
+container.appendChild(button);
 
 
 // create square's container
@@ -14,19 +18,34 @@ squareContainer.classList.add("square-container");
 container.appendChild(squareContainer);
 
 // create grid
-for (let i = 0; i < squarePerSide * squarePerSide; i++) {
-    const div = document.createElement("div");
-    div.classList.add("square");
-    div.style.width = `${960 / squarePerSide}px`;
-    div.style.height = `${960 / squarePerSide}px`;
-    squareContainer.appendChild(div);
-}
-
-//add effect to the grid
-squareContainer.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = randomColor();
+createSquares(16);
+hovering();
+button.addEventListener("click", () => {
+    let squarePerSide = prompt("The number of squares per side for the new grid");
+    removeSquares(squarePerSide);
+    createSquares(squarePerSide);
+    hovering();
 });
 
+
+function createSquares(size) {
+
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        div.id = "square";
+        div.style.width = `${960 / size}px`;
+        div.style.height = `${960 / size}px`;
+        squareContainer.appendChild(div);
+    }
+}
+
+
+//add effect to the grid
+function hovering() {
+    squareContainer.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = randomColor();
+    });
+}
 
 //function random color
 function randomColor() {
@@ -36,4 +55,10 @@ function randomColor() {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
+}
+
+//function remove web
+function removeSquares(size) {
+    const div = document.querySelectorAll("#square");
+    div.forEach(x => x.remove());
 }
